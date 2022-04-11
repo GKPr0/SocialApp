e19adedb5f1c974fa0ad99e8da2c942c5544e2a2
Ondřej Vacek <ondrej.vacek@entry-cz.com>
2022-04-11 22:11:17 +0200

    Added validation and error handling to backend and frontend

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+
+
+export const history = createBrowserHistory();
 
 
 const container = document.getElementById('root');
 render(<React.StrictMode>
         <StoreContext.Provider value={store}>
-          <BrowserRouter>
+          <Router history={history}>
             <App/>
-          </BrowserRouter>
+          </Router>
         </StoreContext.Provider>
       </React.StrictMode>,
       container);
